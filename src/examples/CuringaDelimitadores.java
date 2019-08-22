2d9e95ac279f3f0050eebd74e7dc30b9486103a6
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-08-22 20:24:04 -0300

    Curinga Delimitador only Numbers

--- src/examples/CuringaDelimitadores.java
+++ src/examples/CuringaDelimitadores.java
@@ -10,12 +10,9 @@ public class CuringaDelimitadores {
 
 		List<Integer> myInts = Arrays.asList(1, 2, 3, 4, 5);
 		List<Double> myDoubles = Arrays.asList(3.14, 5.84);
-		List<String> myStgs = Arrays.asList("Maria", "Joao");
 		List<Object> myObjs = new ArrayList<>();
 		
 		copy(myInts, myObjs);
-		printList(myObjs);
-		copy(myStgs, myObjs);
 		printList(myObjs);
 		copy(myDoubles, myObjs);
 		printList(myObjs);
@@ -26,8 +23,8 @@ public class CuringaDelimitadores {
 	//Subtipos, entao pode acessar os elementos
 	//Lista de destino pode ser mais generica que Number "CONTRAVARIANCIA" -> supertipos, pode Add
 	//Eh uma lista de qualquer tipo que pode ser um supertipo de Number
-	public static void copy(List<? extends Object> source, List<? super Object> destiny) {
-		for (Object number : source) {
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
 			destiny.add(number);
 		}
 	}
