99d8516ccf1e00bf36ea85891ec577134fec1ac0
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-04 15:38:18 -0300

    Expressoes Lambda

--- src/examples/InterfaceFuncional.java
+++ src/examples/InterfaceFuncional.java
@@ -2,6 +2,7 @@ package examples;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -15,10 +16,23 @@ public class InterfaceFuncional {
 		list.add(new Product("Teclado", 120.00));
 		list.add(new Product("Monitor", 800.00));
 		
-		
-		
-		list.removeIf(new Product());//Poderia ser uma classe aparte que implementa co Predicate<>
+		Predicate<Product> pred = new Predicate<Product>() {
 
+			@Override
+			public boolean test(Product p) {
+				return p.getPrice() >= 100;
+			}
+		};
+		
+		Predicate<Product> pred2 = p -> p.getPrice() >= 100;
+		
+		//list.removeIf(new Product());//Poderia ser uma classe aparte que implementa o Predicate<>
+		//list.removeIf(pred);//Utilizando a interface funcional c/ expressao regular
+		//list.removeIf(Product::nonStaticProductpredicate);//Reference methods S/ Static
+		//list.removeIf(Product::staticProductpredicate);//reference methods C/ static
+		//list.removeIf(pred2);//Expressao lambda funcional
+		
+		list.removeIf(p -> p.getPrice() >= 100);//expressao lambda inLine 
 		
 		list.forEach(System.out::println);
 	}
