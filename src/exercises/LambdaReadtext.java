3687336ba2b8ae5b6d67068b2de69495762b2feb
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-07 13:09:00 -0300

    Stream Product reversed

--- src/exercises/LambdaReadtext.java
+++ src/exercises/LambdaReadtext.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -15,35 +16,34 @@ public class LambdaReadtext {
 
 	public static void main(String[] args)  {
 		
-		Locale.setDefault(Locale.US);
-		Product p = new Product();
-		List<Product> list = new ArrayList<>();
+		Locale.setDefault(Locale.US);		
 		
 		String path = "D:\\ESOFT\\git\\Curso-Java\\in.txt";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			List<Product> list = new ArrayList<>();	
+			
 			String fileLine = br.readLine();
 				while (fileLine != null) {
 					String[] fields = fileLine.split(",");
-					String name = fields[0];
-					double price = Double.parseDouble(fields[1]);
-					p = new Product(name, price);
-					list.add(p);
+					list.add(new Product(fields[0], Double.parseDouble(fields[1])));
 					fileLine = br.readLine();
 			}
 			
-			double sum = 0;
-			for (Product prod : list) {
-				sum += prod.getPrice();
-			}
-			
-			double avg = sum / list.stream().count();
+			double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x, y) -> x + y) / list.size();
 			
 			System.out.println("Averege price: " + String.format("%.2f", avg));
 			
-			List<Product> produtos = list.stream().filter(p1 -> p1.getPrice() <= avg).collect(Collectors.toList());
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
 			
-			produtos.forEach(System.out::println);
+			List<String> names = list.stream()
+					.filter(p -> p.getPrice() < avg)
+					.map(p -> p.getName())
+					.sorted(comp.reversed())
+					.collect(Collectors.toList());
+						
+			names.forEach(System.out::println);
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
