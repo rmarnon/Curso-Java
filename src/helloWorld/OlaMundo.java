909050817bd811dadacdb3f7399f98022c9bfeb8
Rodrigo Marnon Veloso Barros de Morais <rmarnon@yahoo.com.br>
2019-09-14 09:32:23 -0300

    Update 14/09/2019

--- src/helloWorld/OlaMundo.java
+++ src/helloWorld/OlaMundo.java
@@ -6,7 +6,7 @@ public class OlaMundo {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);	
-		System.out.println("Ola Mundo!");
+		System.out.println("Hello Fucking World!");
 		sc.close();
 	}
 	
