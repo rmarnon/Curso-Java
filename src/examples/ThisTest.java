e3b998f4084581987f185f423d4314d065ad48d3
Rodrigo Marnon <rmarnon@yahoo.com.br>
2019-12-16 18:16:48 -0300

    Teste nos construtores que utilizam This na sobrecarga

--- src/examples/ThisTest.java
+++ src/examples/ThisTest.java
@@ -8,7 +8,31 @@ public class ThisTest {//Somente uma classe pode ser publica
 		
 		SimpleTime time = new SimpleTime(15, 47, 58);
 		System.out.println(time.buildString());
+		System.out.println("-----------------------------------------");
+		SimpleTime t1 = new SimpleTime();
+		SimpleTime t2 = new SimpleTime(07);
+		SimpleTime t3 = new SimpleTime(15, 25);
+		SimpleTime t4 = new SimpleTime(21, 41, 51);
+		SimpleTime t5 = new SimpleTime(t4);
+				
+		display("T1: Inicializado", t1);
+		display("T2: Hora", t2);
+		display("T3: Hora/Minuto", t3);
+		display("T4: Hora/Minuto/Segundo", t4);
+		display("T5: Objeto T4", t5);
+		
+		try {
+			SimpleTime t6 = new SimpleTime(27, 74, 98);
+		}
+		catch (IllegalArgumentException e) {
+			System.out.printf("Excessao ao inicializar T6: %s%n", e.getMessage());
+		}
+		
 	}
+	
+	public static void display(String header, SimpleTime t) {
+		System.out.printf("%s%n %s%n %s%n%n", header, t.toUniversalString(), t.toString());
+	}	
 }
 	
 /*****/class SimpleTime {//Classes adicionais nao podem ter encapsulamento!
@@ -29,12 +53,17 @@ public class ThisTest {//Somente uma classe pode ser publica
 		
 		//Construtor "Padrao" sobrecarregado implicitamente c/ (3 argumentos) e inicializandos.
 		public SimpleTime() {
-			this(05,50,55);
+			this(11, 22, 33);
 		}
 		
-		//Construtor obrecarregado c/ Hora fornecida, minuto e segundo padrao 0.
+		//Construtor obrecarregado c/ Hora fornecida, minuto e segundo = 0.
 		public SimpleTime(int hora) {
 			this(hora, 0, 0);
+		}
+		
+		//Sobrecarga com hora/minuto, segundos = 0.
+		public SimpleTime(int hora, int minuto) {
+			this(hora, minuto, 0);
 		}
 		
 		//Invoca construtor com 03 argumentos passando o proprio construtor 
@@ -58,7 +87,7 @@ public class ThisTest {//Somente uma classe pode ser publica
 			 return String.format("%26s: %s%n%26s: %s%n%26s: %s",  
 					 "this.toUniversalString()", this.toUniversalString(),
 					 "toUniversalStringComGet()", toUniversalStringComGet(),
-			 		 "toUSFormat()", toUSFormat());	
+			 		 "toString()", toString());	
 		}
 
 		public String toUniversalString() {//Converte em String o formato (HH:MM:SS)
@@ -69,9 +98,8 @@ public class ThisTest {//Somente uma classe pode ser publica
 			return String.format("%02d:%02d:%02d", getHora() , getMinuto(), getSegundo());
 		}
 		
-		public String toUSFormat() {//Converte em String o formato (HH:MM:SS "AM" ou "PM")
+		public String toString() {//Converte em String o formato (HH:MM:SS "AM" ou "PM")
 			return String.format("%02d:%02d:%02d %s", (hora == 0 || hora == 24)? hora: hora % 12,
 					minuto, segundo, (hora < 12)? "\"AM\"": "\"PM\"");//Sem This implicito (Opcional)
-		}		
+		}			
 	}
-
